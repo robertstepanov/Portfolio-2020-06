2841697b8f6c5013ae03f547c2358a75841749b9
Robert Stepanov <robertdstepanov@gmail.com>
2020-07-18 18:53:32 -0400

    Desktop about done

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name  \n Message: $message";
+$recipient = "robert.d.stepanov@gmail.com";
+$subject = "Portfolio Submission";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" , " -" , "<a href='index.html' style='text-decoration:none; color: #ff0099;'> Return Home</a>";
+
+?>
