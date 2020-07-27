876e7cb55dc1b8759d6623f200418d5622a650b4
Robert <robertdstepanov@gmail.com>
2020-07-27 16:21:55 -0400

    bug fixes for mobile

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
-let mainNavLinks = document.querySelectorAll("nav h4 a");
+// When the user scrolls down 20px from the top of the document, show the button
 
-let mainSections = document.querySelectorAll("section div");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('topBtn').style.display = 'block';
+  } else {
+    document.getElementById('topBtn').style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+let mainNavLinks = document.querySelectorAll('nav h4 a');
+
+let mainSections = document.querySelectorAll('section div');
 
 let lastId;
 let cur = [];
 
-window.addEventListener("scroll", event => {
-    let fromTop = window.scrollY;
+window.addEventListener('scroll', (event) => {
+  let fromTop = window.scrollY;
 
-    mainNavLinks.forEach(link => {
-        let section = document.querySelector(link.hash);
+  mainNavLinks.forEach((link) => {
+    let section = document.querySelector(link.hash);
 
-        if (
-            section.offsetTop <= fromTop + 160 && section.offsetTop + section.offsetHeight >= fromTop + 160
-        )  {
-            link.classList.add("current");
-        } else {
-            link.classList.remove("current");
-        }
-    });
+    if (
+      section.offsetTop <= fromTop + 160 &&
+      section.offsetTop + section.offsetHeight >= fromTop + 160
+    ) {
+      link.classList.add('current');
+    } else {
+      link.classList.remove('current');
+    }
+  });
 });
--- mail.php
+++ mail.php
@@ -7,5 +7,6 @@ $subject = "Portfolio Submission";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" , " -" , "<a href='index.html' style='text-decoration:none; color: #ff0099;'> Return Home</a>";
-
+// echo '<script>
+// alert("Thank you for reaching out.")</script>'
 ?>
